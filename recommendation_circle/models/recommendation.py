1e0c6402b35226e8710bc7903a4701d14546990f
Artur Monteiro <Artur@scopi.com.br>
2022-11-08 01:28:52 -0300

    fix(Recommendations): recommendation model

--- recommendation_circle/models/recommendation.py
+++ recommendation_circle/models/recommendation.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+
+class Recommendation(models.Model):
+    anime = models.CharField(max_length=255)
+    from_user_name = models.CharField(max_length=255)
+    to_user_name = models.CharField(max_length=255)
+
     def __str__(self):
         from_str = self.from_user.name if self.from_user else self.from_user_name
         to_str = self.to_user.name if self.to_user else self.to_user_name
